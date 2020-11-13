42d1529a92dd8fdb24464c0c142865cdc55ce9ab
William Elliman <william.elliman@gmail.com>
2020-11-13 18:35:02 -0500

    Added person parameter to createUser method

--- Project0/src/main/java/dev/elliman/services/PersonService.java
+++ Project0/src/main/java/dev/elliman/services/PersonService.java
@@ -3,5 +3,16 @@ package dev.elliman.services;
 import dev.elliman.beans.Person;
 
 public interface PersonService {
-	
+		//create
+		public Integer createUser(Person person);
+		
+		//read
+		public Person getPersonById(Integer id);
+		public Person getPersonByUsername(String username);
+		
+		//update
+		public void updatePerson(Person p);
+		
+		//delete
+		public void deletePerson(Person p);
 }
