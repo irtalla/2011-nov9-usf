c6e1cd3acbfc1418602eaccb9ec4397bfea58520
William Elliman <william.elliman@gmail.com>
2020-11-13 17:17:24 -0500

    forgot to save the Person class before last commit

--- Project0/src/main/java/dev/elliman/beans/Person.java
+++ Project0/src/main/java/dev/elliman/beans/Person.java
@@ -1,5 +1,12 @@
 package dev.elliman.beans;
 
-public abstract class Person {
-
+public class Person {
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String password;//scary plain text passwords
+	private String role;//employee/customer/manager/etc...
+	
+	
 }
