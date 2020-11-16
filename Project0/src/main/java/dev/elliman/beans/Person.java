286ef73dc1d62b420e794dab52fadcf4396d0729
William Elliman <william.elliman@gmail.com>
2020-11-16 14:50:50 -0500

    added getters and setters for most fields

--- Project0/src/main/java/dev/elliman/beans/Person.java
+++ Project0/src/main/java/dev/elliman/beans/Person.java
@@ -23,4 +23,42 @@ public class Person {
 	public Integer getID() {
 		return id;
 	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+	
+	
 }
