eb1a055101c4c198235f046b64f25e545677b192
William Elliman <william.elliman@gmail.com>
2020-11-16 14:36:02 -0500

    Added DAO methods

--- Project0/src/main/java/dev/elliman/data/PersonDAO.java
+++ Project0/src/main/java/dev/elliman/data/PersonDAO.java
@@ -1,5 +1,18 @@
 package dev.elliman.data;
 
+import java.util.Set;
+
+import dev.elliman.beans.Person;
+
 public interface PersonDAO {
 
+	public Integer add(Person p);
+	
+	public Person getByID(Integer id);
+	
+	public Set<Person> getAll();
+	
+	public void update(Person p);
+	
+	public void delete(Person p);
 }
