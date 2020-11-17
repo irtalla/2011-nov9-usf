e7ec694b7e090c82648ce7142e506381a77b2331
Brodie Hufnagel <brodiehufnagel@gmail.com>
2020-11-17 17:31:31 -0500

    I hope I'm doing this right

--- Week2/CatApp2/src/main/java/com/revature/controllers/PersonController.java
+++ Week2/CatApp2/src/main/java/com/revature/controllers/PersonController.java
@@ -1,8 +1,15 @@
 package com.revature.controllers;
 
+import com.revature.beans.Person;
+import com.revature.exceptions.NonUniqueUsernameException;
+import com.revature.services.PersonService;
+import com.revature.services.PersonServiceImpl;
+
 import io.javalin.http.Context;
 
 public class PersonController {
+	private static PersonService personServ = new PersonServiceImpl();
+	
 	public static void logIn(Context ctx) {
 		
 	}
@@ -11,8 +18,16 @@ public class PersonController {
 		
 	}
 	
-	public static void registerUser(Context ctx) {
-		
+	public static void registerUser(Context ctx){
+		Person newPerson = ctx.bodyAsClass(Person.class);
+		try {
+			personServ.addPerson(newPerson);
+		}
+		catch(NonUniqueUsernameException e){
+			System.out.println("Username already taken. :(");
+			ctx.status(409);
+		}
+		ctx.status(200);
 	}
 	
 	public static void getUserById(Context ctx) {
@@ -24,6 +39,9 @@ public class PersonController {
 	}
 	
 	public static void deleteUser(Context ctx) {
-		
+		Integer id = Integer.valueOf(ctx.pathParam("id"));
+		Person person = personServ.getPersonById(id);
+		personServ.deletePerson(person);
+		ctx.status(200);
 	}
 }
