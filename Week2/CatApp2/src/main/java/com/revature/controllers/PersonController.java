9b41630d262020e8209d7c1f922bc16c9f8463d2
Jason Zelonka <jzelonka@ufl.edu>
2020-11-17 16:44:55 -0500

    person login and person getuserbyid implementation

--- Week2/CatApp2/src/main/java/com/revature/controllers/PersonController.java
+++ Week2/CatApp2/src/main/java/com/revature/controllers/PersonController.java
@@ -1,9 +1,36 @@
 package com.revature.controllers;
 
+import com.revature.beans.Person;
+import com.revature.services.PersonService;
+import com.revature.services.PersonServiceImpl;
+
 import io.javalin.http.Context;
 
 public class PersonController {
+	public static PersonService serv = new PersonServiceImpl();
+	
 	public static void logIn(Context ctx) {
+		String username = ctx.pathParam("user");
+		String password = ctx.pathParam("pass");
+		Person p = serv.getPersonByUsername(username);
+		if (p != null)
+		{
+			if (p.getPassword().equals(password))
+			{
+				ctx.status(200);
+				ctx.json(p);
+			}
+			else
+			{
+				//password mismatch
+				ctx.status(400);
+			}
+		}
+		else
+		{
+			//username not found
+			ctx.status(404);
+		}
 		
 	}
 	
@@ -16,6 +43,15 @@ public class PersonController {
 	}
 	
 	public static void getUserById(Context ctx) {
+		Integer id = Integer.valueOf(ctx.pathParam("id"));
+		Person p = serv.getPersonById(id);
+		if (p != null) {
+			ctx.status(200);
+			ctx.json(p);
+		} else {
+			ctx.status(404);
+		}
+		
 		
 	}
 	
