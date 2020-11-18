b4a91c8f53f2c4cfb1fdf6af2cdccf7ce51dd9cb
William Elliman <william.elliman@gmail.com>
2020-11-18 15:50:13 -0500

    Added the Permission object for easier access to options for users

---
+++ Project0/src/main/java/dev/elliman/beans/Permission.java
@@ -0,0 +1,11 @@
+package dev.elliman.beans;
+
+public class Permission {
+	public final Integer LEVEL;
+	public final String TYPE;
+	
+	public Permission(Integer level, String type) {
+		LEVEL = level;
+		TYPE = type;
+	}
+}
