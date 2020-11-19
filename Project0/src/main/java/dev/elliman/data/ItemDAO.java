9c824968ea6b9b4a3739acc2643656686fbaf0a2
William Elliman <william.elliman@gmail.com>
2020-11-19 10:11:57 -0500

    Added method stubs for ItemDAO

--- Project0/src/main/java/dev/elliman/data/ItemDAO.java
+++ Project0/src/main/java/dev/elliman/data/ItemDAO.java
@@ -1,5 +1,18 @@
 package dev.elliman.data;
 
-public interface ItemDAO {
+import java.util.Set;
 
+import dev.elliman.beans.Item;
+
+public interface ItemDAO {
+	
+	public Integer add(Item p);
+	
+	public Item getByID(Integer id);
+	
+	public Set<Item> getAll();
+	
+	public void update(Item p);
+	
+	public void delete(Item p);
 }
