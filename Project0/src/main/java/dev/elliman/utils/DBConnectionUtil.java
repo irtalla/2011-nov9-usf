dbaba456da79937a69becacae1d27c8a23d7b103
William Elliman <william.elliman@gmail.com>
2020-11-20 12:00:13 -0500

    added database connection parts

---
+++ Project0/src/main/java/dev/elliman/utils/DBConnectionUtil.java
@@ -0,0 +1,47 @@
+package dev.elliman.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class DBConnectionUtil {
+	private static DBConnectionUtil dbcu = null;
+	private static Properties properties;
+	
+	//Singleton design pattern
+	private DBConnectionUtil() {
+		properties = new Properties();
+		
+		try {
+			InputStream dbProperties = DBConnectionUtil.class.getClassLoader().getSystemResourceAsStream("database.properties");
+			properties.load(dbProperties);
+		} catch (IOException e) {
+			
+		}
+		
+	}
+	
+	public static synchronized DBConnectionUtil getDBConnectionUtil() {
+		if(dbcu == null) {
+			dbcu = new DBConnectionUtil();
+		}
+		return dbcu;
+	}
+	
+	public Connection getConnection() {
+		Connection conn = null;
+		
+		try {
+			Class.forName(properties.getProperty("drv"));
+			conn = DriverManager.getConnection(
+					properties.getProperty("url"),
+					properties.getProperty("user"),
+					properties.getProperty("password"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+}
