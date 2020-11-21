b3a1c55f6c99e49ac434311c0816eb700fd80546
William Elliman <william.elliman@gmail.com>
2020-11-20 19:10:29 -0500

    added OfferDAO

---
+++ Project0/src/main/java/dev/elliman/data/OfferDAO.java
@@ -0,0 +1,29 @@
+package dev.elliman.data;
+
+import java.util.Set;
+
+import dev.elliman.beans.Bike;
+import dev.elliman.beans.Offer;
+import dev.elliman.beans.Person;
+
+public interface OfferDAO {
+	public Integer makeOffer(Offer offer);
+	
+	public void update(Offer offer);
+	
+	public void rejectAll(Set<Offer> offers);
+	
+	public Set<Offer> getOffers(Person person);
+	
+	public Set<Offer> getActiveOffers(Person person);
+	
+	public Set<Offer> getOffers(Bike bike);
+	
+	public Set<Offer> getActiveOffers(Bike bike);
+	
+	public Set<Offer> getAllOffers();
+	
+	public Set<Offer> getAllActiveOffers();
+	
+	public Offer getById(Integer id);
+}
