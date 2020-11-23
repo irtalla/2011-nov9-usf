04bf1405976577f9e758fdfd8440de30d7c162c3
William Elliman <william.elliman@gmail.com>
2020-11-23 18:11:57 -0500

    added equals and hashCode

--- Project0/src/main/java/dev/elliman/beans/Offer.java
+++ Project0/src/main/java/dev/elliman/beans/Offer.java
@@ -45,4 +45,53 @@ public class Offer {
 	public String getStatus() {
 		return status;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((bike == null) ? 0 : bike.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((person == null) ? 0 : person.hashCode());
+		result = prime * result + ((price == null) ? 0 : price.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Offer other = (Offer) obj;
+		if (bike == null) {
+			if (other.bike != null)
+				return false;
+		} else if (!bike.equals(other.bike))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (person == null) {
+			if (other.person != null)
+				return false;
+		} else if (!person.equals(other.person))
+			return false;
+		if (price == null) {
+			if (other.price != null)
+				return false;
+		} else if (!price.equals(other.price))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		return true;
+	}
 }
