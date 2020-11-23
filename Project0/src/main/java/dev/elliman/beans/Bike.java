69b26606d579f8fa11f0274bbf41a17b027cda62
William Elliman <william.elliman@gmail.com>
2020-11-23 16:24:51 -0500

    added equals and hashCode

--- Project0/src/main/java/dev/elliman/beans/Bike.java
+++ Project0/src/main/java/dev/elliman/beans/Bike.java
@@ -40,4 +40,49 @@ public class Bike {
 	public Integer getOwner() {
 		return ownerId;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((model == null) ? 0 : model.hashCode());
+		result = prime * result + ((ownerId == null) ? 0 : ownerId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bike other = (Bike) obj;
+		if (color == null) {
+			if (other.color != null)
+				return false;
+		} else if (!color.equals(other.color))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (model == null) {
+			if (other.model != null)
+				return false;
+		} else if (!model.equals(other.model))
+			return false;
+		if (ownerId == null) {
+			if (other.ownerId != null)
+				return false;
+		} else if (!ownerId.equals(other.ownerId))
+			return false;
+		return true;
+	}
+	
+	
 }
