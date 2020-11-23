453de64cd34f5d86c38a8cc2483d169921eb5d13
William Elliman <william.elliman@gmail.com>
2020-11-23 10:48:06 -0500

    added the empty implementation

---
+++ Project0/src/main/java/dev/elliman/services/BikeServiceImpl.java
@@ -0,0 +1,45 @@
+package dev.elliman.services;
+
+import java.util.Set;
+
+import dev.elliman.beans.Bike;
+
+public class BikeServiceImpl implements BikeService {
+
+	@Override
+	public Integer addBike(Bike bike) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bike getById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Bike> getAvalibleBikes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bike getByModel(String model) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Bike bike) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Bike bike) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
