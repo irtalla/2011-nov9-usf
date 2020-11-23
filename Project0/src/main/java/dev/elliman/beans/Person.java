97ba26443dafa76e3521c1300db590108ab6dc48
William Elliman <william.elliman@gmail.com>
2020-11-23 11:28:14 -0500

    added equals and hashCode methods

--- Project0/src/main/java/dev/elliman/beans/Person.java
+++ Project0/src/main/java/dev/elliman/beans/Person.java
@@ -91,4 +91,36 @@ public class Person {
 			}
 		}
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((ownedBikes == null) ? 0 : ownedBikes.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if(other.getID() == id) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	
 }
