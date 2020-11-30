0a83c6e1cef18fb7eb86d33bd6bae11f5d2e91cf
@vonfunkhouserprod <vonfunkhouserprod@gmail.com>
2020-11-30 10:40:11 -0700

    Provide basic implementation.

---
+++ FizzBuzz/index.js
@@ -0,0 +1,23 @@
+const list = document.getElementById("numberList");
+const choiceDisplay = document.getElementById("choice");
+const choiceInput = document.getElementById("choice-input");
+choiceInput.addEventListener("change", handleChangeToNumber);
+
+function handleChangeToNumber(event){
+    const number = parseInt(event.target.value);
+    choiceDisplay.innerHTML = `Your number: ${number}`;
+
+    for(i = 1; i <= number; i++){
+        let innerHtml = i;
+        if(i % 3 == 0){
+            innerHtml = "Fizz";
+        }else if(i % 5 == 0){
+            innerHtml = "Buzz";
+        }
+
+        const newListItem = document.createElement("li");
+        newListItem.innerHTML = "" + innerHtml;
+        
+        list.appendChild(newListItem);
+    }
+}
