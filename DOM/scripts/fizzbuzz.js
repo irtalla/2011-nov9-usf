c87fe8f8b0e08a0e0a9aa05fea9b761a8675024a
jnimmo <Tristan_Nimmo@msn.com>
2020-12-01 10:22:28 -0500

    A DOM that works!

--- DOM/scripts/fizzbuzz.js
+++ DOM/scripts/fizzbuzz.js
@@ -3,22 +3,22 @@
 function fizzbuzz() {
 
     var userInput = document.getElementById("input").value;
-    console.log(userInput);
+    //  console.log(userInput);
     
     for (let i = 1; i <= userInput; i++ ) {
-        let para = document.createElement("p");
+        var para = document.createElement("p");
         if ( i % 3 == 0 && i % 5 == 0) {
-        para.innerHTML = "FIZZBUZZ";
-        para.appendChild("myDiv");
+            para.innerHTML = "FIZZBUZZ";
+            document.getElementById("myDiv").appendChild(para);
         } else if (i % 3 == 0)  {
             para.innerHTML = "FIZZ";
-            para.appendChild("myDiv");
+            document.getElementById("myDiv").appendChild(para);
         } else if  (i % 5 == 0) {
             para.innerHTML = "Buzz";
-            para.appendChild("myDiv");
+            document.getElementById("myDiv").appendChild(para);
         }  else {
             para.innerHTML = i;
-            para.appendChild("myDiv");
+            document.getElementById("myDiv").appendChild(para);
         }
     }
 }
