0969874c26bde98738c73ecef6844b1c39743323
William Elliman <william.elliman@gmail.com>
2020-12-02 17:52:42 -0500

    updated Person to include all current DB info

--- Project1/src/main/java/dev/elliman/beans/Person.java
+++ Project1/src/main/java/dev/elliman/beans/Person.java
@@ -1,12 +1,30 @@
 package dev.elliman.beans;
 
 public class Person {
+	private Integer id;
 	private String username;
 	private String password;
+	private String firstName;
+	private String lastName;
+	private Double amountClaimed;
+	private Role role;
 	
 	public Person() {
+		id = null;
 		username = null;
 		password = null;
+		firstName = null;
+		lastName = null;
+		amountClaimed = null;
+		role = null;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
 	public String getUsername() {
@@ -24,6 +42,36 @@ public class Person {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Double getAmountClaimed() {
+		return amountClaimed;
+	}
+
+	public void setAmountClaimed(Double amountClaimed) {
+		this.amountClaimed = amountClaimed;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
 }
