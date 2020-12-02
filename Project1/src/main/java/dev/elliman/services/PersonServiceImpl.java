c65924fab1c5e61f90c443a9f92b0a9d7926bd84
William Elliman <william.elliman@gmail.com>
2020-12-02 18:12:45 -0500

    added the class for a person service

---
+++ Project1/src/main/java/dev/elliman/services/PersonServiceImpl.java
@@ -0,0 +1,11 @@
+package dev.elliman.services;
+
+public class PersonServiceImpl implements PersonService {
+
+	@Override
+	public boolean login(String username, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
