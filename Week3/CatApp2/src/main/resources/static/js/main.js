eaf6432faebc8bcbe2414b2fb28a00e828859fc4
kyledwilly <kyledwilly@hotmail.com>
2020-12-02 12:21:44 -0800

    changing user / changing pass push

--- Week3/CatApp2/src/main/resources/static/js/main.js
+++ Week3/CatApp2/src/main/resources/static/js/main.js
@@ -14,7 +14,7 @@ function setNav() {
                 <input id="user" name="user" type="text" />
                 <label for="pass"> Password: </label>
                 <input id="pass" name="pass" type="password" />
-                <button type="button" id="loginBtn">Log In</button>
+                <button type="button" id="loginBtn">Log In</button> 
             </form>
         `;
     } else {
@@ -22,7 +22,8 @@ function setNav() {
             <a href="myCats.html">My Cats</a>
             <span>
                 ${loggedUser.username}&nbsp;
-                <button type="button" id="loginBtn">Log Out</button>
+                <button type="button" id="loginBtn">Log Out</button><br>
+                <button type="button" id="updateBtn">Update User</button>
             </span>
         `;
     }
@@ -30,6 +31,9 @@ function setNav() {
     let loginBtn = document.getElementById('loginBtn');
     if (loggedUser) loginBtn.onclick = logout;
     else loginBtn.onclick = login;
+
+    let updateBtn = document.getElementById('updateBtn');
+    updateBtn.onclick = update;
 }
 
 async function login() {
@@ -73,4 +77,51 @@ async function checkLogin() {
     let response = await fetch(url);
     if (response.status === 200) loggedUser = await response.json();
     setNav();
+}
+
+async function update() {
+    let url = baseUrl + '/users';
+    // let response = await fetch(url);
+    // if (response.status === 200) loggedUser = await response.json();
+    // setNav();
+
+
+    nav.innerHTML += `<form>
+    <label for="updateUser">Username: </label>
+    <input id="updateUser" name="user" type="text" />
+    <label for="updatePass">Password: </label>
+    <input id="updatePass" name="pass" type="password" />
+    <button type="button" id="updateSubmitBtn">Update User</button>
+    </form>`;
+
+    let updateSubmitBtn = document.getElementById('updateSubmitBtn');
+    updateSubmitBtn.onclick = updateSubmit;
+    
+
+    async function updateSubmit(){
+        let usr = {};
+        usr.username = document.getElementById('updateUser').value;
+        usr.password = document.getElementById('updatePass').value;
+
+        const options = {
+            method: 'PUT',
+            body: JSON.stringify(usr),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+            let url = baseUrl + '/users/' + loggedUser.id;
+            let response = await fetch(url, options);
+
+            switch (response.status) {
+                case 202: // successful
+                   //loggedUser = await response.json();
+                    setNav();
+                    break;
+
+                default: // other error
+                    alert('Something went wrong.');
+                    break;
+            }
+    }
 }
