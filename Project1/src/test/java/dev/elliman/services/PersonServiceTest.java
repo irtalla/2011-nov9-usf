3f98f4449e82672c8b5fa22fa2aaa86e8348b4c3
William Elliman <william.elliman@gmail.com>
2020-12-02 18:16:40 -0500

    Login test fails successfully

--- Project1/src/test/java/dev/elliman/services/PersonServiceTest.java
+++ Project1/src/test/java/dev/elliman/services/PersonServiceTest.java
@@ -1,5 +1,52 @@
 package dev.elliman.services;
 
-public class PersonServiceTest {
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import dev.elliman.beans.Person;
+import dev.elliman.beans.Role;
+import dev.elliman.data.PersonDAO;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+	
+	@Mock
+	private static PersonDAO personDAO;
+	
+	@InjectMocks
+	private static PersonServiceImpl personServ;
+	
+	private static Person testPerson;
+	
+	@BeforeAll
+	public static void beforeAll() {
+		testPerson = new Person();
+		testPerson.setId(0);
+		testPerson.setUsername("test");
+		testPerson.setPassword("password");
+		testPerson.setFirstName("first");
+		testPerson.setLastName("last");
+		testPerson.setAmountClaimed(0D);
+		Role role = new Role();
+		role.setId(0);
+		role.setName("role");
+		testPerson.setRole(role);
+	}
+	
+	@Test
+	public void testLogin() {
+		when(personDAO.getPersonByUsername(testPerson.getUsername())).thenReturn(testPerson);
+		
+		assertTrue(personServ.login(testPerson.getUsername(), testPerson.getPassword()));
+		
+		verify(personDAO).getPersonByUsername(testPerson.getUsername());
+	}
 }
