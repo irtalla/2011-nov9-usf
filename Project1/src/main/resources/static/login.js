23c80a845b7e014be55426c35c28ac5a8ef1ec8e
William Elliman <william.elliman@gmail.com>
2020-12-02 17:02:34 -0500

    updated design and added js call

--- Project1/src/main/resources/static/login.js
+++ Project1/src/main/resources/static/login.js
@@ -1,3 +1,6 @@
+var loginBtn = document.getElementById("loginBtn")
+loginBtn.onclick = login;
+
 function login() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
