99590b14e2c844090a5587226cd1177b05b28a64
William Elliman <william.elliman@gmail.com>
2020-12-02 18:26:32 -0500

    added login() impl

--- Project1/src/main/java/dev/elliman/services/PersonServiceImpl.java
+++ Project1/src/main/java/dev/elliman/services/PersonServiceImpl.java
@@ -1,11 +1,30 @@
 package dev.elliman.services;
 
+import dev.elliman.beans.Person;
+import dev.elliman.data.PersonDAO;
+import dev.elliman.data.PersonDAOFactory;
+
 public class PersonServiceImpl implements PersonService {
+	
+	private PersonDAO personDAO;
+	
+	public PersonServiceImpl() {
+		personDAO = PersonDAOFactory.getPersonDAO();
+	}
 
 	@Override
 	public boolean login(String username, String password) {
-		// TODO Auto-generated method stub
-		return false;
+		Person p = personDAO.getPersonByUsername(username);
+		
+		if(p == null) {
+			return false;
+		}
+		
+		if(password.equals(p.getPassword())) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
