f2b9cbfa92779b6cf226b8f9cabde2a8f841665f
Do Yeun Kim <doyeunkim.vm@gmail.com>
2020-12-02 09:41:52 -0500

    initialized AdditionalFile.java in com.revature.models

---
+++ Project1/src/main/java/com/revature/models/AdditionalFile.java
@@ -0,0 +1,12 @@
+package com.revature.models;
+
+public class AdditionalFile {
+	private Integer id;
+	private String name;
+	private String content;
+	
+	public AdditionalFile() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
