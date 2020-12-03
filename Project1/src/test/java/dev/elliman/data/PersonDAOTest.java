c0de7fb6a3174ba9bcf0870863141da4d7202135
William Elliman <william.elliman@gmail.com>
2020-12-02 19:10:26 -0500

    setup tests for DAOs

---
+++ Project1/src/test/java/dev/elliman/data/PersonDAOTest.java
@@ -0,0 +1,36 @@
+package dev.elliman.data;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import dev.elliman.beans.Person;
+import dev.elliman.beans.Role;
+
+public class PersonDAOTest {
+	
+	private static PersonDAO personDAO;
+
+	private static Person testPerson;
+	
+	@BeforeAll
+	public static void beforeAll() {
+		personDAO = PersonDAOFactory.getPersonDAO();
+		
+		testPerson = new Person();
+		testPerson.setId(0);
+		testPerson.setUsername("test");
+		testPerson.setPassword("password");
+		testPerson.setFirstName("first");
+		testPerson.setLastName("last");
+		testPerson.setAmountClaimed(0D);
+		Role role = new Role();
+		role.setId(1);
+		role.setName("role");
+		testPerson.setRole(role);
+	}
+	
+	@Test
+	public void getPersonByUsername() {
+		
+	}
+}
