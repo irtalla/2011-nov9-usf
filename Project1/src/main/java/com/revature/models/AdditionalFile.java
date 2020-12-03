b8716e56076b96ee083576bcba85cdb81cf2b84d
Do Yeun Kim <doyeunkim.vm@gmail.com>
2020-12-03 10:56:27 -0500

    tentative changes to AdditionalFile.java in com.revature.models

--- Project1/src/main/java/com/revature/models/AdditionalFile.java
+++ Project1/src/main/java/com/revature/models/AdditionalFile.java
@@ -3,7 +3,6 @@ package com.revature.models;
 public class AdditionalFile {
 	private Integer id;
 	private String name;
-	private String content;
 	
 	public AdditionalFile() {
 		// TODO Auto-generated constructor stub
