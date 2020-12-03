fcebb4e5b8cc6caeb09aea6711c7884c8e16239a
Do Yeun Kim <doyeunkim.vm@gmail.com>
2020-12-03 10:57:12 -0500

    initializing the front-end side

---
+++ Project1/src/main/resources/static/js/main.js
@@ -0,0 +1,69 @@
+"use strict"
+
+let baseUrl = 'http://localhost:8080';
+let nav = document.getElementById('navBar');
+let loggedUser = null;
+
+checkLogin();
+
+function setNav() {
+    nav.innerHTML = ``;
+    if (loggedUser) {
+        nav.innerHTML += ``;
+    } else {
+        nav.innerHTML += ``;
+    }
+
+    // let loginBtn = document.getElementById('loginBtn');
+    // if (loggedUser) {
+    //     loginBtn.onclick = logout;
+    // } else {
+    //     loginBtn.onclick = login;
+    // }
+}
+
+async function login() {
+    let url = baseUrl + '/users?';
+    url += 'user=' + document.getElementById('user').value + '&';
+    url += 'pass=' + document.getElementById('pass').value;
+    let response = await fetch(url, {method:'PUT'});
+
+    switch (response.status) {
+        case 200:
+            loggedUser = await response.json();
+            setNav();
+            break;
+        case 400:
+            alert('Incorrect password. Try again.');
+            document.getElementById('pass').value = '';
+            break;
+        case 404:
+            alert('Username not found.');
+            document.getElementById('user').value = '';
+            document.getElementById('pass').value = '';
+            break;
+        default:
+            alert('Unexpected error occurred.');
+            break;
+    }
+}
+
+async function logout() {
+    let url = baseUrl + '/users';
+    let response = await fetch(url, {method:'DELETE'});
+
+    if (response.status !== 200) {
+        alert('Failed to logout');
+    }
+    loggedUser = null;
+    setNav();
+}
+
+async function checkLogin() {
+    let url = baseUrl + '/users';
+    let response = await fetch(url);
+    if (response.status === 200) {
+        loggedUser = await response.json();
+    }
+    setNav();
+}
