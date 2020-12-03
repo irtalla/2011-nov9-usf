f4e667f45e1b6bea78542cb74e685434309d6ad1
William Elliman <william.elliman@gmail.com>
2020-12-03 14:11:06 -0500

    added misspelled Attachment bean

---
+++ Project1/src/main/java/dev/elliman/beans/Atachment.java
@@ -0,0 +1,83 @@
+package dev.elliman.beans;
+
+import java.io.File;
+
+public class Atachment {
+	private Integer id;
+	private Integer claimID;
+	private File file;
+	
+	public Atachment() {
+		id = null;
+		claimID = null;
+		file = null;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getClaimID() {
+		return claimID;
+	}
+
+	public void setClaimID(Integer claimID) {
+		this.claimID = claimID;
+	}
+
+	public File getFile() {
+		return file;
+	}
+
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((claimID == null) ? 0 : claimID.hashCode());
+		result = prime * result + ((file == null) ? 0 : file.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Atachment other = (Atachment) obj;
+		if (claimID == null) {
+			if (other.claimID != null)
+				return false;
+		} else if (!claimID.equals(other.claimID))
+			return false;
+		if (file == null) {
+			if (other.file != null)
+				return false;
+		} else if (!file.equals(other.file))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Atachment [id=" + id + ", claimID=" + claimID + ", file=" + file + "]";
+	}
+	
+	
+}
