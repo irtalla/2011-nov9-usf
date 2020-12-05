ca0e9a711eb7925c1d9588f577390e33339108ca
Do Yeun Kim <doyeunkim.vm@gmail.com>
2020-12-05 16:52:30 -0500

    First draft of UserHibernate and JUnit test for DAO complete

--- Project1/src/main/java/com/revature/data/UserHibernatePostgres.java
+++ Project1/src/main/java/com/revature/data/UserHibernatePostgres.java
@@ -1,9 +1,12 @@
 package com.revature.data;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 import com.revature.exceptions.InvalidEmailException;
 import com.revature.exceptions.NonUniqueEmailException;
@@ -46,44 +49,119 @@ public class UserHibernatePostgres implements UserDAO {
 	
 	@Override
 	public User getById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		User u = null;
+		
+		try (Session s = session.getCurrentSession()) {
+			s.beginTransaction();
+			u = s.get(User.class, id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return u;
 	}
 
 	@Override
 	public User getByUsername(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		User u = null;
+		
+		try (Session s = session.getCurrentSession()) {
+			s.beginTransaction();
+			String hql = "FROM User WHERE username = '" + username + "'";
+			List<User> resultList = s.createQuery(hql, User.class).list();
+			u = resultList.get(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return u;
 	}
 
 	@Override
 	public User getByEmail(String email) {
-		// TODO Auto-generated method stub
-		return null;
+		User u = null;
+		
+		try (Session s = session.getCurrentSession()) {
+			s.beginTransaction();
+			String hql = "FROM User WHERE email = '" + email + "'";
+			List<User> resultList = s.createQuery(hql, User.class).list();
+			u = resultList.get(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return u;
 	}
 	
 	@Override
 	public Set<User> getByRole(Role role) {
-		// TODO Auto-generated method stub
-		return null;
+		Set<User> users = null;
+		
+		try (Session s = session.getCurrentSession()) {
+			s.beginTransaction();
+			String hql = "FROM User WHERE role_id = " + role.getId();
+			List<User> resultList = s.createQuery(hql, User.class).list();
+			users = new HashSet<User>(resultList);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return users;
 	}
 
 	@Override
 	public Set<User> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		Set<User> users = null;
+		
+		try (Session s = session.getCurrentSession()) {
+			s.beginTransaction();
+			String hql = "FROM User";
+			List<User> resultList = s.createQuery(hql, User.class).list();
+			users = new HashSet<User>(resultList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return users;
 	}
 	
 	@Override
 	public void update(User t) throws NonUniqueUsernameException, NonUniqueEmailException, InvalidEmailException {
-		// TODO Auto-generated method stub
+		try (Session s = session.getCurrentSession()){
+			s.beginTransaction();
+			s.update(t);
+			s.getTransaction().commit();
+		} catch (Exception e) {
+			if (e.getMessage().contains("violates unique constraint")) {
+				if (e.getMessage().contains("username")) {
+					throw new NonUniqueUsernameException();
+				} else if (e.getMessage().contains("email")) {
+					throw new NonUniqueEmailException();
+				} else {
+					e.printStackTrace();
+				}
+			} else if (e.getMessage().contains("violates check constraint")) {
+				if (e.getMessage().contains("email")) {
+					throw new InvalidEmailException();
+				} else {
+					e.printStackTrace();
+				}
+			}
+			e.printStackTrace();
+		}
 
 	}
 	
 	@Override
 	public void delete(User t) {
-		// TODO Auto-generated method stub
-
+		try (Session s = session.getCurrentSession()){
+			s.beginTransaction();
+			s.delete(t);
+			s.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 
