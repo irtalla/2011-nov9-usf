560d6f73d6ad3fd0c92becaca261d6914da37480
Robert Richter <RichterJRobert@gmail.com>
2020-12-07 16:11:03 -0500

    first controller attempt

---
+++ Project1/src/main/java/com/revature/app/SPMSJavalinApp.java
@@ -0,0 +1,49 @@
+package com.revature.app;
+import static io.javalin.apibuilder.ApiBuilder.*;
+
+import io.javalin.Javalin;
+
+public class SPMSJavalinApp {
+	
+	public static void main(String[] args) {
+		Javalin app = Javalin.create((config) -> {
+			config.addStaticFiles("/static"); // pulling from src/main/resources
+			config.enableCorsForAllOrigins();
+		});
+		
+		app.start(8080);
+		
+		app.routes(() -> {
+			
+			path("users", () -> {
+				get(PersonController::checkLogin); // get logged in user
+				put(PersonController::logIn); // log in user
+				post(PersonController::registerUser); // register new user
+				delete(PersonController::logOut); // log out user
+				path (":id", () -> {
+					get(PersonController::getUserById); // get user by id
+					put(PersonController::updateUser); // update user
+					delete(PersonController::deleteUser); // delete user
+				});
+			});
+			
+		
+			path("pitch", () -> { 
+				post(PitchController::addCat); 
+				path ("all", () -> {
+					get(PitchController::getAllPitches); 
+				});
+				path(":id", () -> {
+					get(PitchController::getPitchById); 
+					put(PitchController::updatePitch); 
+					delete(PitchController::deletePitch);
+				});
+			});
+
+
+			path("Committee", () -> {
+				get(CommitteeController::getCommittee); 
+			});
+		});
+	}
+}
--- Project1/src/main/java/com/revature/service/PersonService.java
+++ Project1/src/main/java/com/revature/service/PersonService.java
@@ -4,10 +4,11 @@ import java.util.Set;
 
 import com.revature.beans.Person;
 import com.revature.beans.Pitch;
+import com.revature.exceptions.NonUniqueUsernameException;
 
 public interface PersonService {
 	// create
-	public Integer addPerson(Person p);
+	public Integer addPerson(Person p) throws NonUniqueUsernameException;
 	// read
 	public Person getPersonById(Integer id);
 	public Person getPersonByUsername(String username);
