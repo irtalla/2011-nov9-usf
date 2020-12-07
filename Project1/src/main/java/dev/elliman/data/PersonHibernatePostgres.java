4ce8846b68ab1b059827b5e25933049c72b8e963
William Elliman <william.elliman@gmail.com>
2020-12-07 11:40:56 -0500

    added hibernate DAO for person

---
+++ Project1/src/main/java/dev/elliman/data/PersonHibernatePostgres.java
@@ -0,0 +1,31 @@
+package dev.elliman.data;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Session;
+
+import dev.elliman.beans.Person;
+import dev.elliman.utils.HibernateUtil;
+
+public class PersonHibernatePostgres implements PersonDAO {
+	private HibernateUtil hu = HibernateUtil.getHibernateUtil();
+
+	@Override
+	public Person getPersonByUsername(String username) {
+		Session s = hu.getSession();
+		CriteriaBuilder cb = s.getCriteriaBuilder();
+		CriteriaQuery<Person> criteria = cb.createQuery(Person.class);
+		Root<Person> root = criteria.from(Person.class);
+		
+		Predicate predicateForUsername = cb.equal(root.get("username"), username);
+		
+		criteria.select(root).where(predicateForUsername);
+		
+		Person p = s.createQuery(criteria).getSingleResult();
+		return p;
+	}
+
+}
