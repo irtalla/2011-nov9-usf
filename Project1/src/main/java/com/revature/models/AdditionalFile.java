9b438cefac4b9ec2314bc8ad69bb84e3f9d37f5d
Do Yeun Kim <doyeunkim.vm@gmail.com>
2020-12-09 12:48:14 -0500

    updated AdditionalFile to better reflect the db and file structure

--- Project1/src/main/java/com/revature/models/AdditionalFile.java
+++ Project1/src/main/java/com/revature/models/AdditionalFile.java
@@ -1,14 +1,28 @@
 package com.revature.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name="add_file")
 public class AdditionalFile {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	@Column(name="name")
 	private String name;
-	private String contents; //TODO: find a better data type/structure to store large files
+	@Transient
+	private String path; 
 	
 	public AdditionalFile() {
 		id = 0;
 		name = "";
-		contents = "";
+		path = "";
 	}
 
 	public Integer getId() {
@@ -27,21 +41,21 @@ public class AdditionalFile {
 		this.name = name;
 	}
 
-	public String getContents() {
-		return contents;
+	public String getPath() {
+		return path;
 	}
 
-	public void setContents(String contents) {
-		this.contents = contents;
+	public void setPath(String path) {
+		this.path = path;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((contents == null) ? 0 : contents.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((path == null) ? 0 : path.hashCode());
 		return result;
 	}
 
@@ -54,11 +68,6 @@ public class AdditionalFile {
 		if (getClass() != obj.getClass())
 			return false;
 		AdditionalFile other = (AdditionalFile) obj;
-		if (contents == null) {
-			if (other.contents != null)
-				return false;
-		} else if (!contents.equals(other.contents))
-			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -69,12 +78,17 @@ public class AdditionalFile {
 				return false;
 		} else if (!name.equals(other.name))
 			return false;
+		if (path == null) {
+			if (other.path != null)
+				return false;
+		} else if (!path.equals(other.path))
+			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "AdditionalFile [id=" + id + ", name=" + name + ", contents=" + contents + "]";
+		return "AdditionalFile [id=" + id + ", name=" + name + ", path=" + path + "]";
 	}
 
 }
