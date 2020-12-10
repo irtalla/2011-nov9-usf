e809dbe7ecd7878998261ace77b4bd4d254a4db7
Do Yeun Kim <doyeunkim.vm@gmail.com>
2020-12-10 11:46:56 -0500

    updated AdditionalFile to remove unnecessary field

--- Project1/src/main/java/com/revature/models/AdditionalFile.java
+++ Project1/src/main/java/com/revature/models/AdditionalFile.java
@@ -15,13 +15,11 @@ public class AdditionalFile {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	@Column(name="file_name")
-	private String name;
-	@Transient
-	private String path; 
+	private String path;
+
 	
 	public AdditionalFile() {
 		id = 0;
-		name = "";
 		path = "";
 	}
 
@@ -31,14 +29,6 @@ public class AdditionalFile {
 
 	public void setId(Integer id) {
 		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public String getPath() {
@@ -54,7 +44,6 @@ public class AdditionalFile {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		result = prime * result + ((path == null) ? 0 : path.hashCode());
 		return result;
 	}
@@ -73,11 +62,6 @@ public class AdditionalFile {
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
 		if (path == null) {
 			if (other.path != null)
 				return false;
@@ -88,7 +72,7 @@ public class AdditionalFile {
 
 	@Override
 	public String toString() {
-		return "AdditionalFile [id=" + id + ", name=" + name + ", path=" + path + "]";
+		return "AdditionalFile [id=" + id + ", path=" + path + "]";
 	}
 
 }
