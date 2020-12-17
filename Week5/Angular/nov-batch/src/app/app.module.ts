1b47b8ff93674fbc51255b346e43d64f18ba24ec
Lydia  Bierer <lbierer@gmail.com>
2020-12-17 15:06:59 -0600

    more

--- Week5/Angular/nov-batch/src/app/app.module.ts
+++ Week5/Angular/nov-batch/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SierraComponent } from './sierra/sierra.component';
+import { LydiaComponent } from './lydia/lydia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SierraComponent
+    SierraComponent,
+    LydiaComponent
   ],
   imports: [
     BrowserModule,
