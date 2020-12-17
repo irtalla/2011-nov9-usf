512ca28a4aa28ea1627bbb9229c250d46f0cdcae
Sierra Nicholes <s.k.nicholes@gmail.com>
2020-12-17 15:53:05 -0500

    Added all

--- Week5/Angular/nov-batch/src/app/app.module.ts
+++ Week5/Angular/nov-batch/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SierraComponent } from './sierra/sierra.component';
+import AssociateService from './associate.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { SierraComponent } from './sierra/sierra.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AssociateService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
