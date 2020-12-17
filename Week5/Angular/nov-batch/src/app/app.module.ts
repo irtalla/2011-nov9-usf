c9865d1a42550d6cfcc66a08ccb08bfbc7f85689
dwar102 <david_warrington@outlook.com>
2020-12-17 15:44:02 -0500

    my component

--- Week5/Angular/nov-batch/src/app/app.module.ts
+++ Week5/Angular/nov-batch/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SierraComponent } from './sierra/sierra.component';
+import { DavidComponent } from './david/david.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SierraComponent
+    SierraComponent,
+    DavidComponent
   ],
   imports: [
     BrowserModule,
---
+++ Week5/Angular/nov-batch/src/app/david/david.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DavidComponent } from './david.component';
+
+describe('DavidComponent', () => {
+  let component: DavidComponent;
+  let fixture: ComponentFixture<DavidComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DavidComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DavidComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
